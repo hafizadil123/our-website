a8bd6edc90fbc2afda0826443af98a06ed1e9f40
Hamza Rafique <hamzarafique964@gmail.com>
2023-10-19 20:56:51 +0500

    features hero video section content updated

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -12,7 +12,7 @@ const Features = () => {
         <div className="container">
           <SectionTitle
             title="Main Features"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            paragraph="We offer a wide range of software development services to help you grow your business."
             center
           />
 
